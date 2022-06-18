9261a6d2e0f5856b7be5670676f25646d2405084
Jaewon <woin2ee@gmail.com>
2022-06-18 19:47:29 +0900

    프로젝트 기본 설정

--- RealmExample/MainViewController.swift
+++ RealmExample/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  RealmExample
 //
 //  Created by Jaewon on 2022/06/18.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
